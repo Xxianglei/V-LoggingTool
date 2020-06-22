71c1c2170bdbe1c3fac2b36bf2b62023a6ecc744
xianglei <1767925069@qq.com>
2020-06-22 16:33:55 +0800

    update aop

--- src/main/java/com/xianglei/aspect/AuditLogAspect.java
+++ src/main/java/com/xianglei/aspect/AuditLogAspect.java
@@ -15,6 +15,7 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -33,7 +34,7 @@ import java.lang.reflect.Method;
 @Slf4j
 @Aspect
 @Component
-@Order(Integer.MIN_VALUE)
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class AuditLogAspect {
     @Autowired
     AuditLogService logService;
--- src/main/java/com/xianglei/config/LogAsyncConfiguration.java
+++ src/main/java/com/xianglei/config/LogAsyncConfiguration.java
@@ -21,7 +21,7 @@ public class LogAsyncConfiguration implements AsyncConfigurer {
     @Autowired
     LogThreadConfigProperties logThreadConfigProperties;
 
-    @Bean
+    @Bean(name = "VLogThreadPool")
     public Executor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(logThreadConfigProperties.getCorePoolSize());
